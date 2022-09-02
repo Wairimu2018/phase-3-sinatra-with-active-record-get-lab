82b26eac48058ee2942a130ce39b1ab8bdfaced7
Wairimu2018 <wairimukuria.wk@gmail.com>
2022-09-02 21:20:31 +0300

    push code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+
 
 end
